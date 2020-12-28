82f5b76990b6929c78cdf105237f78822b8c27b3
brice <briceguard-git@yahoo.com>
2020-12-28 16:50:08 +0000

    make symlink for headers

--- Sources/RZUtilsTouch/include/UIImage+ImageWithUIView.h
+++ Sources/RZUtilsTouch/include/UIImage+ImageWithUIView.h
@@ -1,13 +1,1 @@
-//
-//  UIImage+ImageWithUIView.h
-//
-
-#import <UIKit/UIKit.h>
-#import <QuartzCore/QuartzCore.h>
-
-@interface UIImage (ImageWithUIView)
-+ (UIImage *)imageWithUIView:(UIView *)view;
--(UIImage*)mergeWithImage:(UIImage*)second;
-
-
-@end
+../foundation/UIImage+ImageWithUIView.h
